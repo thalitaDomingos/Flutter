dbce1b954eaf7e9df8cb435d1ea1e77ede3dd512
thalitaDomingos <90002483+thalitaDomingos@users.noreply.github.com>
2023-03-24 19:32:24 -0300

    Melhorias

--- Dependencia_Google_Fonts/lib/main.dart
+++ Dependencia_Google_Fonts/lib/main.dart
@@ -8,7 +8,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -32,39 +31,28 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 1000;
 
-  void _decrementCounter() {
-    setState(() {
-      _counter--;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.blue,
         title: Text("Thalita", style: GoogleFonts.ptSans()),
       ),
-      body: Center(
+      body: Container(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              "Contador:",
-              style: TextStyle(
-                  fontSize: 22.0, color: Color.fromARGB(255, 124, 5, 5)),
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+                backgroundColor: Colors.green,
+                onPressed: () => print(_counter++),
+                child: const Icon(Icons.add)),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _decrementCounter,
-        tooltip: 'Decrement',
-        child: const Icon(Icons.south_outlined),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          backgroundColor: Colors.red,
+          onPressed: () => print(_counter--),
+          child: const Icon(Icons.south_outlined)),
     );
   }
 }
